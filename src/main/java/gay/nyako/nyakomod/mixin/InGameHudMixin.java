80956dae2a6e6d7776e0dbf0643ce86f5b9f5617
NyakoFox <nyakowofox@gmail.com>
2021-12-10 17:24:55 -0400

    1.18.1, remove italics and lore styles

---
+++ src/main/java/gay/nyako/nyakomod/mixin/InGameHudMixin.java
@@ -0,0 +1,20 @@
+package gay.nyako.nyakomod.mixin;
+
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
+import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(InGameHud.class)
+public abstract class InGameHudMixin {
+
+    @Redirect(method= "renderHeldItemTooltip(Lnet/minecraft/client/util/math/MatrixStack;)V", at=@At(value="INVOKE", target="Lnet/minecraft/item/ItemStack;hasCustomName()Z"))
+    public boolean redirect(ItemStack stack) {
+        // Never turn italic
+        return false;
+    }
+}
