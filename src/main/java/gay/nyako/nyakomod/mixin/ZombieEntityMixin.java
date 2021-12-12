cf07be4c74161cbcb075a19d317a867937e954c9
NyakoFox <nyakowofox@gmail.com>
2021-12-12 01:18:03 -0400

    make villagers always convert to zombies on normal

---
+++ src/main/java/gay/nyako/nyakomod/mixin/ZombieEntityMixin.java
@@ -0,0 +1,17 @@
+package gay.nyako.nyakomod.mixin;
+
+import net.minecraft.entity.mob.ZombieEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Random;
+
+@Mixin(ZombieEntity.class)
+public abstract class ZombieEntityMixin {
+
+    @Redirect(method= "onKilledOther(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/LivingEntity;)V", at=@At(value="INVOKE", target="Ljava/util/Random;nextBoolean()Z"))
+    private boolean redirect(Random random) {
+        return true;
+    }
+}
