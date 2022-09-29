9fa81a73503e4be193ba9715729ef00bcd657b76
NyakoFox <nyakowofox@gmail.com>
2022-09-29 13:50:40 -0300

    milkin it up

--- src/main/java/gay/nyako/nyakomod/NyakoAdvancementsGenerator.java
+++ src/main/java/gay/nyako/nyakomod/NyakoAdvancementsGenerator.java
@@ -2,6 +2,7 @@ package gay.nyako.nyakomod;
 
 import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.AdvancementFrame;
+import net.minecraft.advancement.criterion.ConsumeItemCriterion;
 import net.minecraft.advancement.criterion.InventoryChangedCriterion;
 import net.minecraft.item.Items;
 import net.minecraft.predicate.entity.EntityPredicate;
@@ -14,36 +15,41 @@ public class NyakoAdvancementsGenerator implements Consumer<Consumer<Advancement
 
     @Override
     public void accept(Consumer<Advancement> consumer) {
-        // This is our advancement
-        // The difference:
 
         Advancement parentAdvancement = Advancement.Builder.create()
                 .display(NyakoItems.DIAMOND_COIN_ITEM, Text.literal("Cunkin' it up"),
                         Text.translatable("Get your first coin"),
                         new Identifier("textures/gui/advancements/backgrounds/husbandry.png"),
                         AdvancementFrame.TASK,
-                        // whether to show a toast that appears on the top-right corner
                         true,
-                        // whether to announce to chat
                         true,
-                        // whether its hidden in the advancement tab
                         false)
                 .criterion("inventory_changed", InventoryChangedCriterion.Conditions.items(NyakoItems.COPPER_COIN_ITEM))
                 .build(consumer, "nyakomod/root");
 
-        Advancement exampleBlockAdvancement = Advancement.Builder.create()
+        Advancement firstMilkAdvancement = Advancement.Builder.create()
+                .display(Items.MILK_BUCKET, Text.literal("Bone Generator"),
+                        Text.translatable("Drink a refreshing bucket of milk"),
+                        null,
+                        AdvancementFrame.TASK,
+                        true,
+                        true,
+                        false
+                )
+                .parent(parentAdvancement)
+                .criterion("consume_item", ConsumeItemCriterion.Conditions.item(Items.MILK_BUCKET))
+                .build(consumer, "nyakomod/drank_milk");
+
+        Advancement getMilkedAdvancement = Advancement.Builder.create()
                 .display(Items.MILK_BUCKET, Text.literal("Get milked bozo"),
                         Text.translatable("Get milked by another player"),
                         null,
                         AdvancementFrame.TASK,
-                        // whether to show a toast that appears on the top-right corner
                         true,
-                        // whether to announce to chat
                         true,
-                        // whether its hidden in the advancement tab
                         false
                 )
-                .parent(parentAdvancement)
+                .parent(firstMilkAdvancement)
                 .criterion("player_milked", PlayerMilkCriterion.Conditions.any())
                 .build(consumer, "nyakomod/player_milked");
     }
