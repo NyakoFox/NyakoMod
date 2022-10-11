f2139272997e716155abeb3f3468caad0f949ce1
stupid cat <cat@blargbot.xyz>
2022-10-11 12:17:43 -0600

    change pet sprite renderer

--- src/main/java/gay/nyako/nyakomod/entity/renderer/PetSpriteRenderer.java
+++ src/main/java/gay/nyako/nyakomod/entity/renderer/PetSpriteRenderer.java
@@ -1,12 +1,10 @@
 package gay.nyako.nyakomod.entity.renderer;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import gay.nyako.nyakomod.NyakoClientMod;
 import gay.nyako.nyakomod.NyakoMod;
 import gay.nyako.nyakomod.entity.PetSpriteEntity;
-import net.minecraft.client.render.OverlayTexture;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
@@ -14,6 +12,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Matrix3f;
 import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.math.Vec3f;
+import org.lwjgl.opengl.GL11;
 
 public class PetSpriteRenderer extends EntityRenderer<PetSpriteEntity> {
     public static final Identifier TEXTURE = new Identifier("nyakomod", "textures/entity/dragon.png");
@@ -36,52 +35,57 @@ public class PetSpriteRenderer extends EntityRenderer<PetSpriteEntity> {
         float h = 0.0625f;
         matrices.scale(h, h, h);
 
-        VertexConsumer vertexConsumer;
-        try {
-            vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityCutout(getTexture(entity)));
-        } catch (NullPointerException ex) {
-            ex.printStackTrace();
-            vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityCutout(TEXTURE));
-        }
 
-        renderSide(matrices, vertexConsumer, light, false, entity.getPetSize());
-        renderSide(matrices, vertexConsumer, light, true, entity.getPetSize());
+        RenderSystem.setShaderTexture(0, getTexture(entity));
+
+        renderSide(matrices, light, false, entity.getPetSize());
+        renderSide(matrices, light, true, entity.getPetSize());
 
         matrices.pop();
         super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
     }
 
-    public void renderSide(MatrixStack matrices, VertexConsumer consumer, int light, boolean reversed, float petSize) {
+    public void renderSide(MatrixStack matrices, int light, boolean reversed, float petSize) {
         matrices.push();
         MatrixStack.Entry entry = matrices.peek();
-        Matrix4f matrix4f = entry.getPositionMatrix();
+        Matrix4f matrix = entry.getPositionMatrix();
         Matrix3f matrix3f = entry.getNormalMatrix();
 
         if (reversed) {
-            matrices.multiply(Vec3f.NEGATIVE_Y.getDegreesQuaternion(180f));
+             matrices.multiply(Vec3f.NEGATIVE_Y.getDegreesQuaternion(180f));
         }
 
-//        int size = Math.max((int)(16 * petSize), 2);
         int size = (int)(16 * petSize);
 
         int width = size;
         int height = size;
 
-        if (reversed) {
-            this.vertex(matrix4f, matrix3f, consumer, -width / 2, 0, 0, 0, 1, light);
-            this.vertex(matrix4f, matrix3f, consumer, -width / 2, height, 0, 0, 0, light);
-            this.vertex(matrix4f, matrix3f, consumer, width / 2, height, 0, 1, 0, light);
-            this.vertex(matrix4f, matrix3f, consumer, width / 2, 0, 0, 1, 1, light);
-        } else {
-            this.vertex(matrix4f, matrix3f, consumer, -width / 2, 0, 0, 1, 1, light);
-            this.vertex(matrix4f, matrix3f, consumer, -width / 2, height, 0, 1, 0, light);
-            this.vertex(matrix4f, matrix3f, consumer, width / 2, height, 0, 0, 0, light);
-            this.vertex(matrix4f, matrix3f, consumer, width / 2, 0, 0, 0, 1, light);
-        }
-        matrices.pop();
-    }
+        var x0 = -width / 2;
+        var x1 = width / 2;
+        var y0 = 0;
+        var y1 = height;
+        var z = 0;
 
-    public void vertex(Matrix4f positionMatrix, Matrix3f normalMatrix, VertexConsumer vertexConsumer, int x, int y, int z, float u, float v, int light) {
-        vertexConsumer.vertex(positionMatrix, x, y, z).color(255, 255, 255, 0).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, 0f, 1f, 0f).next();
+        var u0 = 1;
+        var u1 = 0;
+        var v0 = 1;
+        var v1 = 0;
+        if (reversed) {
+            u0 = 0;
+            u1 = 1;
+        }
+
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.enableDepthTest();
+        RenderSystem.depthFunc(GL11.GL_LEQUAL);
+        RenderSystem.enableCull();
+        BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+        bufferBuilder.vertex(matrix, x0, y1, z).texture(u0, v1).light(light).next();
+        bufferBuilder.vertex(matrix, x1, y1, z).texture(u1, v1).light(light).next();
+        bufferBuilder.vertex(matrix, x1, y0, z).texture(u1, v0).light(light).next();
+        bufferBuilder.vertex(matrix, x0, y0, z).texture(u0, v0).light(light).next();
+        BufferRenderer.drawWithShader(bufferBuilder.end());
+        matrices.pop();
     }
 }
