4ab7414d0c3cd0793afaa11cf7b759e06b14535f
NyakoFox <nyakowofox@gmail.com>
2022-10-12 15:09:08 -0300

    change fns+ a bit

--- src/main/java/gay/nyako/nyakomod/NyakoItems.java
+++ src/main/java/gay/nyako/nyakomod/NyakoItems.java
@@ -92,7 +92,7 @@ public class NyakoItems {
     public static final Item ALLYBOX                    = register("allybox",                   new BlockItem(NyakoBlocks.ALLYBOX, new FabricItemSettings().group(ItemGroup.MISC)));
     public static final Item DRAGON_MILK_BUCKET         = register("dragon_milk_bucket",        new Item(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.UNCOMMON)));
     public static final Item MONITOR                    = register("monitor",                   new MonitorItem(new FabricItemSettings().group(ItemGroup.DECORATIONS).maxCount(64)));
-    public static final Item FLINT_AND_STEEL_PLUS       = register("flint_and_steel_plus",      new FlintAndSteelPlusItem(new FabricItemSettings().maxDamage(64).group(ItemGroup.TOOLS)));
+    public static final Item FLINT_AND_STEEL_PLUS       = register("flint_and_steel_plus",      new FlintAndSteelPlusItem(new FabricItemSettings().maxDamage(16).group(ItemGroup.TOOLS)));
     public static final Item CREEPER_HEAD               = register("creeper",                   new BlockItem(NyakoBlocks.CREEPER, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
 
     public static final Item MUSIC_DISC_WOLVES          = NyakoDiscs.WOLVES.item();
