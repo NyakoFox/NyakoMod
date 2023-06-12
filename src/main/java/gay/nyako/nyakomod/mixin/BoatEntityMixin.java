07445f0b73e838e4c690a061481917e736e56a1f
NyakoFox <nyakowofox@gmail.com>
2023-06-12 13:31:17 -0300

    allow boats to jump in water

--- src/main/java/gay/nyako/nyakomod/mixin/BoatEntityMixin.java
+++ src/main/java/gay/nyako/nyakomod/mixin/BoatEntityMixin.java
@@ -4,8 +4,10 @@ import gay.nyako.nyakomod.access.BoatEntityAccess;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,6 +15,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BoatEntity.class)
 public abstract class BoatEntityMixin extends Entity implements BoatEntityAccess {
     private boolean pressingJump = false;
+
+    @Shadow
+    protected abstract boolean checkBoatInWater();
 
     public BoatEntityMixin(EntityType<?> type, World world) {
         super(type, world);
@@ -30,9 +35,10 @@ public abstract class BoatEntityMixin extends Entity implements BoatEntityAccess
 
     @Inject(method = "updatePaddles()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V", shift = At.Shift.AFTER))
     private void injected(CallbackInfo ci) {
-        if (((BoatEntityAccess) this).isPressingJump() && this.isOnGround())
+        if (((BoatEntityAccess) this).isPressingJump() && (this.isOnGround() || this.checkBoatInWater()))
         {
-            this.addVelocity(0, 0.35, 0);
+            Vec3d velocity = this.getVelocity();
+            this.setVelocity(velocity.x, 0.35, velocity.z);
         }
     }
 }
