7f5802514dbf6fb393439cb3fa1c524df522b624
stupid cat <cat@blargbot.xyz>
2023-07-12 13:59:09 -0600

    Adjust damage sources

--- src/main/java/gay/nyako/nyakomod/NyakoDamageSources.java
+++ src/main/java/gay/nyako/nyakomod/NyakoDamageSources.java
@@ -3,7 +3,7 @@ package gay.nyako.nyakomod;
 import net.minecraft.entity.damage.DamageSource;
 
 public class NyakoDamageSources {
-    public static final DamageSource TOTEM_OF_DYING = new DamageSource("totemOfDying");
-    public static final DamageSource KILLBIND = new DamageSource("killbind");
-    public static final DamageSource EAT_PICKAXE = new DamageSource("eatPickaxe");
+    public static final DamageSource TOTEM_OF_DYING = new DamageSource("totemOfDying").setBypassesArmor().setOutOfWorld();
+    public static final DamageSource KILLBIND = new DamageSource("killbind").setBypassesArmor().setOutOfWorld();
+    public static final DamageSource EAT_PICKAXE = new DamageSource("eatPickaxe").setBypassesArmor().setUnblockable();
 }
