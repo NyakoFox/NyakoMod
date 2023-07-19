fcf7586aff27e4e463721d27c0a607db0fc56993
NyakoFox <nyakowofox@gmail.com>
2023-07-19 12:57:21 -0300

    Fix #13

--- src/main/java/gay/nyako/nyakomod/mixin/BlocksMixin.java
+++ src/main/java/gay/nyako/nyakomod/mixin/BlocksMixin.java
@@ -9,12 +9,18 @@ import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(Blocks.class)
 public class BlocksMixin {
+
+    /**
+     * @author LlamaLad7
+     * @reason Makes the end portal frame drop itself, by making dropsNothing() do... nothing.
+     */
+
     @Redirect(
             method = "<clinit>",
             slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=end_portal_frame")),
             at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractBlock$Settings;dropsNothing()Lnet/minecraft/block/AbstractBlock$Settings;", ordinal = 0)
     )
-    private static AbstractBlock.Settings bedrockDropsItself(AbstractBlock.Settings instance) {
+    private static AbstractBlock.Settings endPortalFrameDropsItself(AbstractBlock.Settings instance) {
         return instance;
     }
 }
