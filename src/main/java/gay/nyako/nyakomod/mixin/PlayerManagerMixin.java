0dfe05db593541afd77db6cde4325c0dcf288b37
NyakoFox <nyakowofox@gmail.com>
2023-10-23 11:45:01 -0300

    re-add fakecount

--- src/main/java/gay/nyako/nyakomod/mixin/PlayerManagerMixin.java
+++ src/main/java/gay/nyako/nyakomod/mixin/PlayerManagerMixin.java
@@ -1,11 +1,9 @@
 package gay.nyako.nyakomod.mixin;
 
-import net.minecraft.advancement.Advancement;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.WorldProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
