b2f49ab13a5dd8a6c91bc8d2a192adaffc8b746a
NyakoFox <nyakowofox@gmail.com>
2025-08-16 04:25:56 -0300

    Add NBT conditions to coin values

--- src/main/java/gay/nyako/nyakomod/CoinDataResourceReloadListener.java
+++ src/main/java/gay/nyako/nyakomod/CoinDataResourceReloadListener.java
@@ -1,5 +1,6 @@
 package gay.nyako.nyakomod;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import gay.nyako.nyakomod.data.CunkCoinData;
@@ -23,7 +24,7 @@ public class CoinDataResourceReloadListener implements SimpleSynchronousResource
 
         manager.findResources("coin_values", identifier -> identifier.getPath().endsWith(".json")).forEach((resourceId, resource) -> {
             try {
-                var shopId = new Identifier(
+                var entityId = new Identifier(
                         resourceId.getNamespace(),
                         resourceId.getPath().substring(12, resourceId.getPath().length() - 5)
                 );
@@ -32,6 +33,8 @@ public class CoinDataResourceReloadListener implements SimpleSynchronousResource
                 JsonObject coinJson = JsonParser.parseReader(new InputStreamReader(resource.getInputStream())).getAsJsonObject();
                 long coinValue = coinJson.get("value").getAsLong();
                 var cunkCoinData = new CunkCoinData(coinValue);
+
+                // Load dimension multipliers
                 JsonObject dimensionMultipliers = coinJson.getAsJsonObject("dimension_multipliers");
                 if (dimensionMultipliers != null) {
                     dimensionMultipliers.entrySet().forEach(entry -> {
@@ -41,7 +44,20 @@ public class CoinDataResourceReloadListener implements SimpleSynchronousResource
                         cunkCoinData.addMultiplier(dimensionIdentifier, multiplier);
                     });
                 }
-                CunkCoinDataValues.add(shopId, cunkCoinData);
+
+                // Load NBT conditions
+                JsonArray nbtConditions = coinJson.getAsJsonArray("nbt_conditions");
+                if (nbtConditions != null) {
+                    for (int i = 0; i < nbtConditions.size(); i++) {
+                        JsonObject condition = nbtConditions.get(i).getAsJsonObject();
+                        String key = condition.get("key").getAsString();
+                        var value = condition.get("value").getAsString();
+                        double multiplier = condition.get("multiplier").getAsDouble();
+                        cunkCoinData.addNBTCondition(key, value, multiplier);
+                    }
+                }
+
+                CunkCoinDataValues.add(entityId, cunkCoinData);
             } catch (Exception e) {
                 NyakoMod.LOGGER.error("Error occurred while loading resource json " + resourceId, e);
             }
